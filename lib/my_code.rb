7524d7f3971f10864eac8b4918dc70a2b64e7c67
Kyle Luke <th.oldschool@gmail.com>
2019-07-30 01:20:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,46 @@
 # Your Code Here
+
+def map(source_array)
+  i = 0 
+  new_array = []
+  while i<source_array.length do
+    new_array.push( yield(source_array[i]) ) 
+    new_array[i]
+    i += 1
+  end
+  p new_array
+end
+
+
+map([1, 2, 3, -9]) { |n| n }
+
+map([1, 2, 3, -9]) { |n| n * -1 }
+
+map([1, 2, 3, -9]) { |n| n * n }
+
+
+
+def reduce(source_array, starting_point=nil)
+  i = 0 
+  total = 0
+  
+  if starting_point 
+    total = total + starting_point
+  end
+  
+  while i<source_array.length do
+    yield(total, source_array[i]) 
+    total
+    i += 1
+  end
+  p total
+end
+
+
+reduce([1, 2, 3], 100) { |totes, n| totes + n }
+
+
+
+
+
+
