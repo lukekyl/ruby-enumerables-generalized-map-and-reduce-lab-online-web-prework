49fa08d4307d959bbccf399462b90d527f4f8e92
Kyle Luke <th.oldschool@gmail.com>
2019-07-30 02:36:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -30,7 +30,7 @@ def reduce(source_array, starting_point=nil)
   
   while i<source_array.length do 
     total = yield(total, source_array[i]) 
-    if source_array[i] == true
+    if total == true
       return true
     end
     p total
